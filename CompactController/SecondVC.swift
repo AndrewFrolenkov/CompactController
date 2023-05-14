4dd742734d57c672d54528f544c73a025626263e
Andrew Frolenkov <andrewissuess@yahoo.com>
2023-05-15 01:47:40 +0300

    Cempleted

---
+++ CompactController/SecondVC.swift
@@ -0,0 +1,119 @@
+//
+//  SecondVC.swift
+//  CompactController
+//
+//  Created by Андрей Фроленков on 13.05.23.
+//
+
+import Foundation
+import UIKit
+
+protocol ConstraintDelegate: AnyObject {
+  
+  func updateConstraint(int: CGFloat)
+}
+
+class SecondVC: UIViewController {
+  
+  weak var delegate: ConstraintDelegate?
+  
+  lazy var dissmissButton: UIButton = {
+    let button = UIButton()
+    button.setImage(UIImage(systemName: "xmark.circle"), for: .normal)
+    button.tintColor = .black
+    return button
+  }()
+  
+  lazy var segmentedControl: UISegmentedControl = {
+    let segmentedControl = UISegmentedControl(items: ["280pt", "150pt"])
+    segmentedControl.selectedSegmentIndex = 0
+    return segmentedControl
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.9725490196, blue: 0.9921568627, alpha: 1)
+    view.layer.cornerRadius = 10
+    setupConstraints()
+    
+    drawView()
+    
+    dissmissButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    segmentedControl.addTarget(self, action: #selector(segmentedControlTapped), for: .valueChanged)
+  }
+
+  func drawView() {
+    
+    //Path part
+    let path = UIBezierPath()
+    path.lineCapStyle = .round
+    path.move(to: CGPoint(x: 300 / 2 - 10, y: 0)) //StartPoint
+    path.addLine(to: CGPoint(x: 150, y: -10)) //EndPoint of First Line and StartPoint for Second Line
+    path.addLine(to: CGPoint(x: 300 / 2 + 10, y: 0)) //EndPoint of Second Line
+    path.addLine(to: CGPoint(x: 300 / 2 - 10, y: 0)) //EndPoint of Second Line
+    //Shape part
+    let shape = CAShapeLayer()
+    shape.path = path.cgPath
+    shape.lineWidth = 2.0
+    shape.fillColor = #colorLiteral(red: 0.968627451, green: 0.9725490196, blue: 0.9921568627, alpha: 1).cgColor
+    shape.strokeColor = #colorLiteral(red: 0.968627451, green: 0.9725490196, blue: 0.9921568627, alpha: 1).cgColor
+    view.layer.addSublayer(shape)
+  }
+  
+  deinit {
+    print("wow")
+  }
+  
+  @objc func segmentedControlTapped(sender: UISegmentedControl) {
+   
+    self.view.translatesAutoresizingMaskIntoConstraints = false
+    switch sender.selectedSegmentIndex {
+      
+    case 0:
+      delegate?.updateConstraint(int: 280)
+      
+    case 1 :
+     
+      delegate?.updateConstraint(int: 150)
+    default:
+      print("error")
+    }
+  }
+  
+  @objc private func buttonTapped() {
+    
+    // First, notify the child that it’s about to be removed
+    self.willMove(toParent: nil)
+
+    // Then, remove the child from its parent
+    self.removeFromParent()
+
+    // Finally, remove the child’s view from the parent’s
+    self.view.removeFromSuperview()
+  
+    
+  
+    
+  }
+  
+  private func setupConstraints() {
+    
+    dissmissButton.translatesAutoresizingMaskIntoConstraints = false
+    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(dissmissButton)
+    view.addSubview(segmentedControl)
+    
+    NSLayoutConstraint.activate([
+      dissmissButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+      dissmissButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+      dissmissButton.widthAnchor.constraint(equalToConstant: 20),
+      dissmissButton.heightAnchor.constraint(equalToConstant: 20),
+      
+      segmentedControl.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 10),
+      segmentedControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+      
+    ])
+  }
+  
+}
--- CompactController/ViewController.swift
+++ CompactController/ViewController.swift
@@ -7,13 +7,99 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+  
+  lazy var button: UIButton = {
+    let button = UIButton()
+    button.setTitle("Present", for: .normal)
+    button.setTitleColor(UIColor.systemBlue, for: .normal)
+    return button
+  }()
+  
+  lazy var secondVC: SecondVC = {
+    let secondVC = SecondVC()
+    return secondVC
+  }()
+ 
+  var heightAnchor: NSLayoutConstraint?
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    setupConstraints()
+    
+    
+    heightAnchor = secondVC.view.heightAnchor.constraint(equalToConstant: 280)
+    secondVC.delegate = self
+    
+    
+    button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+  }
+  
+  @objc private func buttonPressed() {
+    openSecondVC()
+  }
+  
+  
+  
+  private func openSecondVC() {
+    secondVC.view.translatesAutoresizingMaskIntoConstraints = false
+    self.addChild(secondVC)
+    self.view.addSubview(secondVC.view)
+    secondVC.didMove(toParent: self)
+    
+    
+    NSLayoutConstraint.activate([
+      
+      secondVC.view.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 10),
+      secondVC.view.widthAnchor.constraint(equalToConstant: 300),
+      heightAnchor!,
+      secondVC.view.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      
+
+    ])
   }
 
+  private func setupConstraints() {
+    
+    button.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(button)
+    
+    NSLayoutConstraint.activate([
+      button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+      button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+      
+    ])
+  }
 
 }
 
+extension ViewController: ConstraintDelegate {
+  
+  func updateConstraint(int: CGFloat) {
+    if int == 150 {
+      
+      self.heightAnchor?.constant = 150
+      UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseOut, .allowUserInteraction]) {
+        
+        self.view.layoutIfNeeded()
+        
+      }
+      
+    } else if int == 280 {
+      
+      self.heightAnchor?.constant = 280
+      UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.curveEaseOut, .allowUserInteraction]) {
+        
+        self.view.layoutIfNeeded()
+        
+        
+      }
+    }
+    
+    
+  }
+  
+}
